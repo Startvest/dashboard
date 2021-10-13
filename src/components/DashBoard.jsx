0544083cc90db7636330bc9d6ad5eb2e3352f867
fortune710 <fortune0208@yahoo.com>
2021-10-13 09:14:19 +0100

    Resolved Merge Conflict

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -9,13 +9,9 @@ import { Investors } from '../pages/Investors'
 import { Tools } from '../pages/Tools';
 import { Messages } from '../pages/Messages';
 import { Documents } from '../pages/Documents';
-<<<<<<< HEAD
 import { Account } from '../pages/Account'
 import { Link } from 'react-router-dom';
-=======
-import { Account } from '../pages/Account';
 import {Campaign} from '../pages/CreateCampaign';
->>>>>>> 70fb085e7e640ca056f16a823b5107b671b03b7f
 
 export function Dashboard(){
     return(
@@ -24,28 +20,19 @@ export function Dashboard(){
                 <IonSplitPane contentId="main">
                     <Menu />
                     <IonRouterOutlet id="main">
-                        <Route path="/" exact={true}>
+                        <Link path="/" exact={true}>
                             <Redirect to="/overview" />
-                        </Route>
+                        </Link>
 
-<<<<<<< HEAD
                         <Link path="/overview" exact="true" component={Overview}></Link>
                         <Link path="/analytics" exact="true" component={Analytics}></Link>
                         <Link path="/investors" exact="true" component={Investors}></Link>
                         <Link path="/tools" exact="true" component={Tools}></Link>
-                        <Link path="/messages" exact="true" component={Messages}></Link>
+                        {/*<Link path="/messages" exact="true" component={Messages}></Link>*/}
                         <Link path="/documents" exact="true" component={Documents}></Link>
                         <Link path="/my-account" exact="true" component={Account}></Link>
-=======
-                        <Route path="/overview" exact="true" component={Overview}></Route>
-                        <Route path="/analytics" exact="true" component={Analytics}></Route>
-                        <Route path="/investors" exact="true" component={Investors}></Route>
-                        <Route path="/tools" exact="true" component={Tools}></Route>
-                        {/* <Route path="/messages" exact="true" component={Messages}></Route> */}
-                        <Route path="/campaign" exact="true" component={Campaign}></Route>
-                        <Route path="/documents" exact="true" component={Documents}></Route>
-                        <Route path="/my-account" exact="true" component={Account}></Route>
->>>>>>> 70fb085e7e640ca056f16a823b5107b671b03b7f
+                        <Link path="/campaign" exact="true" component={Campaign}></Link>
+                        
                     </IonRouterOutlet>
 
 
