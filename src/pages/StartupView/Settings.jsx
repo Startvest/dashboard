518baa78a9810d98a4f90a822c4a887488733272
fortune710 <fortune0208@yahoo.com>
2022-02-26 03:00:34 +0100

    Added segment on settings page

--- src/pages/StartupView/Settings.jsx
+++ src/pages/StartupView/Settings.jsx
@@ -1,13 +1,31 @@
-import './styles/Settings.css';
+import styles from './styles/Settings.module.css';
 import Image from '../../assets/images/unsplash_kWgKV2i9Xec.png';
-import { IonContent, IonPage, IonToolbar, IonHeader, IonImg, IonAvatar, IonIcon } from "@ionic/react";
+import { useEffect, useState, useRef } from 'react';
+import { IonContent, IonPage, IonToolbar, IonHeader, IonImg, IonInput,
+    IonAvatar, IonSlide, IonSlides, IonSegment, IonSegmentButton, IonLabel } from "@ionic/react";
 import { checkmarkCircle, add } from 'ionicons/icons';
 import { Col, Row } from 'react-bootstrap';
 import { CheckCircleFill } from 'react-bootstrap-icons'
 
 export function Settings(){
-
+    const [hasLoaded, setHasLoaded] = useState(false);
     const startupName = "Startup Name"
+    const slideRef = useRef()
+
+    useEffect(()=>{
+        setHasLoaded(!hasLoaded)
+    }, [])
+
+
+    const segments = [
+        {name: "Profile"},
+        {name: "Account"},
+        {name: "Appearance"},
+        {name: "Plan"},
+        {name: "Billing"},
+        {name: "2FA Authentication"},
+
+    ]
 
     return(
         <IonPage className="page">
@@ -31,6 +49,57 @@ export function Settings(){
                         </span>
                     </Col>
                 </Row>
+                <IonSegment onIonChange={(e)=>slideRef.current?.slideTo(e.detail.value)}>
+                    {segments.map((segment,index)=>{
+                        return(
+                            <IonSegmentButton value={index} key={index}>
+                                {segment.name}
+                            </IonSegmentButton>
+                        )
+                    })}
+                </IonSegment>
+
+                <IonSlides ref={slideRef}>
+                    <IonSlide>
+
+                        <div className={styles.profile}>
+                            <div className={styles.profile_section}>
+                                <IonLabel>Business Name</IonLabel>
+                                <IonInput
+                                    placeholder="Enter Business name"/>
+
+                                <IonLabel>Business Email</IonLabel>
+                                <IonInput placeholder="Startup email"/>
+
+                                <IonLabel>Industry</IonLabel>
+                                <IonInput placeholder="Startup email"/>
+
+                            </div>
+                            <div className={styles.profile_section}>
+                                <IonLabel>Business Number</IonLabel>
+                                <IonInput placeholder="Enter Business name"/>
+
+                                <IonLabel>Business Location</IonLabel>
+                                <IonInput placeholder="Startup email"/>
+
+                                <IonLabel>Business Email</IonLabel>
+                                <IonInput placeholder="Startup email"/>
+
+                            </div>
+                        </div>
+
+                    </IonSlide>
+
+                    <IonSlide>
+                        <h1>sbdj</h1>
+                    </IonSlide>
+
+                    <IonSlide>
+                        <h1>ayuyua</h1>
+                    </IonSlide>
+
+                </IonSlides>
+
 
 
             </IonContent>
